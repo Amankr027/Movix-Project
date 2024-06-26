44bacb2518a00808ca53227d3aae283735bfb308
Aman Kumar <97677738+Amankr027@users.noreply.github.com>
2024-06-26 18:12:36 +0530

    Update HeroBanner.jsx

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -5,8 +5,8 @@ import "./style.scss";
 
 import useFetch from "../../../hooks/useFetch";
 
-import Img from "../../../components/lazyLoadImage/Img";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import Img from "../../../components/lazyLoadImage/Img";
 
 const HeroBanner = () => {
     const [background, setBackground] = useState("");
